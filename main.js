172498a1fcd20e3c57913a3e21a053ae24646b2a
Antonio Cahun <cahunantonio1@gmail.com>
2024-03-12 03:34:52 +0100

    initial: initial commit

---
+++ main.js
@@ -0,0 +1,93 @@
+Vue.createApp({
+  data(){
+      return {
+          menu: [
+              {
+                id: 1,
+                image: "Ashen_Estus_Ring.webp",
+                name: "ASHEN ESTUS RING",
+                weight: 30,
+                calories: "800 kcal",
+                squirrels: "30 g",
+                fats: "50 g",
+              },
+              {
+                id: 2,
+                image: "Chloranthy_Ring_29.webp",
+                name: "CHLORANTHY RING",
+                weight: 50,
+                calories: "800 kcal",
+                squirrels: "30 g",
+                fats: "50 g",
+              },
+              {
+                id: 3,
+                image: "Estus_Ring.webp",
+                name: "ESTUS RING",
+                weight: 55,
+                calories: "800 kcal",
+                squirrels: "30 g",
+                fats: "50 g",
+              },
+              {
+                id: 4,
+                image: "Havel28DSIII.webp",
+                name: "HAVEL'S RING",
+                weight: 45,
+                calories: "800 kcal",
+                squirrels: "30 g",
+                fats: "50 g",
+              },
+              {
+                id: 5,
+                image: "Life_Ring_29.webp",
+                name: "LIFE RING",
+                weight: 25,
+                calories: "800 kcal",
+                squirrels: "30 g",
+                fats: "50 g",
+              },
+              {
+                id: 6,
+                image: "Prisoner%27s_Chain.webp",
+                name: "PRISONER'S CHAIN RING",
+                weight: 85,
+                calories: "800 kcal",
+                squirrels: "30 g",
+                fats: "50 g",
+              },
+              {
+                id: 7,
+                image: "Ring_of_Favor.webp",
+                name: "RING OF FAVOR",
+                weight: 65,
+                calories: "800 kcal",
+                squirrels: "30 g",
+                fats: "50 g",
+              },
+              {
+                id: 8,
+                image: "Ring_of_Steel_Protection_29.webp",
+                name: "RING OF STEEL PROTECTION",
+                weight: 95,
+                calories: "800 kcal",
+                squirrels: "30 g",
+                fats: "50 g",
+              },
+              {
+                  id: 9,
+                  image: "Sun_Princess_Ring.webp",
+                  name: "SUN PRINCESS RING",
+                  weight: 95,
+                  calories: "800 kcal",
+                  squirrels: "30 g",
+                  fats: "50 g",
+                },
+            ],
+
+            cart: [
+
+            ]
+      }
+  }
+}).mount('#souls-rings')
